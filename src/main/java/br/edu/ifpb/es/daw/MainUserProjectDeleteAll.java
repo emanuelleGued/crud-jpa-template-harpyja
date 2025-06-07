434e8a6c6ff713fc9dcf76b719784e6a55cb1c5a
emanuelleGued <manugguedes1701@gmail.com>
2025-06-07 12:43:06 -0300

    adicionando users e organizations em user project

--- src/main/java/br/edu/ifpb/es/daw/MainUserProjectDeleteAll.java
+++ src/main/java/br/edu/ifpb/es/daw/MainUserProjectDeleteAll.java
@@ -1,24 +1,45 @@
 package br.edu.ifpb.es.daw;
 
+import br.edu.ifpb.es.daw.dao.ProjectDAO;
+import br.edu.ifpb.es.daw.dao.UserDAO;
 import br.edu.ifpb.es.daw.dao.UserProjectDAO;
+import br.edu.ifpb.es.daw.dao.impl.ProjectDAOImpl;
+import br.edu.ifpb.es.daw.dao.impl.UserDAOImpl;
 import br.edu.ifpb.es.daw.dao.impl.UserProjectDAOImpl;
+import br.edu.ifpb.es.daw.entities.Project;
+import br.edu.ifpb.es.daw.entities.User;
 import br.edu.ifpb.es.daw.entities.UserProject;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
+
 import java.util.List;
 
 public class MainUserProjectDeleteAll {
-    public static void main(String[] args) throws DawException {
-        try(EntityManagerFactory emf = Persistence.createEntityManagerFactory("daw")) {
-            UserProjectDAO userProjectDao = new UserProjectDAOImpl(emf);
 
-            System.out.println("Deletando todos os UserProjects...");
+    public static void main(String[] args) {
+        try (EntityManagerFactory emf = Persistence.createEntityManagerFactory("daw")) {
+            UserProjectDAO userProjectDao = new UserProjectDAOImpl(emf);
+            UserDAO userDao = new UserDAOImpl(emf);
+            ProjectDAO projectDao = new ProjectDAOImpl(emf);
+
             List<UserProject> userProjects = userProjectDao.getAll();
+            System.out.println("Deletando " + userProjects.size() + " associações UserProject");
             for (UserProject userProject : userProjects) {
-                System.out.println("Deletando UserProject: " + userProject.getId());
                 userProjectDao.delete(userProject.getId());
             }
-            System.out.println("Todos os UserProjects foram deletados. Total: " + userProjects.size());
+
+            List<User> users = userDao.getAll();
+            for (User user : users) {
+                userDao.delete(user.getId());
+            }
+
+            List<Project> projects = projectDao.getAll();
+            for (Project project : projects) {
+                projectDao.delete(project.getId());
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
--- src/main/java/br/edu/ifpb/es/daw/MainUserProjectSave.java
+++ src/main/java/br/edu/ifpb/es/daw/MainUserProjectSave.java
@@ -1,31 +1,94 @@
 package br.edu.ifpb.es.daw;
 
-import br.edu.ifpb.es.daw.dao.PersistenciaDawException;
-import br.edu.ifpb.es.daw.dao.UserProjectDAO;
-import br.edu.ifpb.es.daw.dao.impl.UserProjectDAOImpl;
-import br.edu.ifpb.es.daw.entities.UserProject;
-import br.edu.ifpb.es.daw.entities.UserProjectId;
+import br.edu.ifpb.es.daw.dao.*;
+import br.edu.ifpb.es.daw.dao.impl.*;
+import br.edu.ifpb.es.daw.entities.*;
+import br.edu.ifpb.es.daw.entities.enums.OrganizationStatus;
 import br.edu.ifpb.es.daw.entities.enums.ProjectRole;
+import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
+
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 public class MainUserProjectSave {
-    public static void main(String[] args) throws PersistenciaDawException {
-        try(EntityManagerFactory emf = Persistence.createEntityManagerFactory("daw")) {
-            UserProjectDAO dao = new UserProjectDAOImpl(emf);
 
-            UserProjectId id = new UserProjectId();
-            id.setUserId(UUID.randomUUID());
-            id.setProjectId(UUID.randomUUID());
+    public static void main(String[] args) {
+        try (EntityManagerFactory emf = Persistence.createEntityManagerFactory("daw")) {
+            UserDAO userDao = new UserDAOImpl(emf);
+            ProjectDAO projectDao = new ProjectDAOImpl(emf);
+            UserProjectDAO userProjectDao = new UserProjectDAOImpl(emf);
+            OrganizationDAO organizationDao = new OrganizationDAOImpl(emf);
 
-            UserProject userProject = new UserProject();
-            userProject.setId(id);
-            userProject.setRole(ProjectRole.MANAGER);
+            Organization organization = new Organization();
+            organization.setName("Dunddies");
+            organization.setRegistrationDate(LocalDateTime.now());
+            organization.setStatus(OrganizationStatus.ACTIVE);
+            organization.setBusinessSize("MEDIUM");
+            organizationDao.save(organization);
 
-            System.out.println("Salvando: " + userProject);
-            dao.save(userProject);
-            System.out.println("Salvo com ID: " + userProject.getId());
+            User user = new User();
+            user.setId(UUID.randomUUID());
+            user.setName("Jim Halpper");
+            user.setEmail("projeto.jim@email.com");
+            user.setPassword("*********");
+            user.setEmailVerified(true);
+            user.setTermsAgreed(true);
+            userDao.save(user);
+
+            Project project = new Project();
+            project.setId(UUID.randomUUID());
+            project.setName("Web Dundies");
+            project.setKey("PROJ-001");
+            project.setType("DESENVOLVIMENTO");
+            project.setExpiration(LocalDateTime.now().plusMonths(6));
+            project.setOrganization(organization);
+            projectDao.save(project);
+
+            System.out.println("Antes da associação:");
+            System.out.println("User: " + user.getId());
+            System.out.println("Project: " + project.getId());
+
+            try (EntityManager em = emf.createEntityManager()) {
+                EntityTransaction transaction = em.getTransaction();
+                transaction.begin();
+
+                try {
+                    User managedUser = em.merge(user);
+                    Project managedProject = em.merge(project);
+
+                    UserProjectId userProjectId = new UserProjectId(
+                            managedUser.getId(),
+                            managedProject.getId());
+
+                    UserProject userProject = new UserProject();
+                    userProject.setId(userProjectId);
+                    userProject.setRole(ProjectRole.DEVELOPER);
+                    userProject.setUser(managedUser);
+                    userProject.setProject(managedProject);
+
+                    managedUser.addProject(userProject);
+                    managedProject.addUserProject(userProject);
+
+                    em.persist(userProject);
+                    transaction.commit();
+
+                    System.out.println("\nDepois da associação:");
+                    System.out.println("UserProject: " + userProject.getId());
+                    System.out.println("User projects count: " + managedUser.getProjects().size());
+                    System.out.println("Project users count: " + managedProject.getUsers().size());
+
+                } catch (Exception e) {
+                    if (transaction.isActive()) {
+                        transaction.rollback();
+                    }
+                    throw e;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
